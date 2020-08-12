Project-Xtended/packages_apps_Xtensions

Xtensions: Network Traffic Font style [2/2]
Signed-off-by: ROXAANE <bajithottyadvaith99@gmail.com>
Change-Id: Iec65b2fb92d5224168a67dbe50fb5b64c5feab1a

src/com/msm/xtended/fragments/NetworkTraffic.java
@@ -26,6 +26,7 @@ import android.view.View;
 import com.android.settings.SettingsPreferenceFragment;
 import com.msm.xtended.preferences.CustomSeekBarPreference;
 import com.msm.xtended.preferences.SystemSettingSwitchPreference;
+import com.msm.xtended.preferences.SystemSettingListPreference;
 import com.android.settings.Utils;
 import android.util.Log;
 
@@ -39,11 +40,13 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String NETWORK_TRAFFIC_FONT_SIZE  = "network_traffic_font_size";
+    private static final String NETWORK_TRAFFIC_FONT_STYLE = "network_traffic_font_style";
 
     private CustomSeekBarPreference mThreshold;
     private SystemSettingSwitchPreference mNetMonitor;
     private ListPreference mNetTrafficType;
     private CustomSeekBarPreference mNetTrafficSize;
+    private SystemSettingListPreference mNetTrafficFont;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -72,6 +75,11 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
                 Settings.System.NETWORK_TRAFFIC_FONT_SIZE, 21);
         mNetTrafficSize.setValue(NetTrafficSize / 1);
         mNetTrafficSize.setOnPreferenceChangeListener(this);
+	mNetTrafficFont = (SystemSettingListPreference) findPreference(NETWORK_TRAFFIC_FONT_STYLE);
+        int netTrafFont = Settings.System.getInt(resolver,
+                Settings.System.NETWORK_TRAFFIC_FONT_STYLE, 0);
+        mNetTrafficFont.setValue(String.valueOf(netTrafFont));
+        mNetTrafficFont.setOnPreferenceChangeListener(this);
 
         value = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
@@ -102,6 +110,13 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
             int width = ((Integer)objValue).intValue();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NETWORK_TRAFFIC_FONT_SIZE, width);
+            return true;
+	}  else if (preference == mNetTrafficFont) {
+            int netTrafFont = Integer.valueOf((String) objValue);
+            int index = mNetTrafficFont.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.
+                NETWORK_TRAFFIC_FONT_STYLE, netTrafFont);
+            mNetTrafficFont.setSummary(mNetTrafficFont.getEntries()[index]);
             return true;
         } else if (preference == mNetTrafficType) {
             int val = Integer.valueOf((String) objValue);

lucasponez/packages_apps_Legion

src/com/legion/settings/NetworkTraffic.java
@@ -26,6 +26,7 @@ import android.view.View;
 import com.android.settings.SettingsPreferenceFragment;
 import com.legion.settings.preference.CustomSeekBarPreference;
 import com.legion.settings.preference.SystemSettingSwitchPreference;
+import com.legion.settings.preference.SystemSettingListPreference;
 import com.android.settings.Utils;
 import android.util.Log;
 
@@ -39,11 +40,13 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String NETWORK_TRAFFIC_FONT_SIZE  = "network_traffic_font_size";
+    private static final String NETWORK_TRAFFIC_FONT_STYLE = "network_traffic_font_style";
 
     private CustomSeekBarPreference mThreshold;
     private SystemSettingSwitchPreference mNetMonitor;
     private ListPreference mNetTrafficType;
     private CustomSeekBarPreference mNetTrafficSize;
+    private SystemSettingListPreference mNetTrafficFont;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -72,6 +75,11 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
                 Settings.System.NETWORK_TRAFFIC_FONT_SIZE, 21);
         mNetTrafficSize.setValue(NetTrafficSize / 1);
         mNetTrafficSize.setOnPreferenceChangeListener(this);
+	mNetTrafficFont = (SystemSettingListPreference) findPreference(NETWORK_TRAFFIC_FONT_STYLE);
+        int netTrafFont = Settings.System.getInt(resolver,
+                Settings.System.NETWORK_TRAFFIC_FONT_STYLE, 0);
+        mNetTrafficFont.setValue(String.valueOf(netTrafFont));
+        mNetTrafficFont.setOnPreferenceChangeListener(this);
 
         value = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
@@ -102,6 +110,13 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
             int width = ((Integer)objValue).intValue();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NETWORK_TRAFFIC_FONT_SIZE, width);
+            return true;
+	}  else if (preference == mNetTrafficFont) {
+            int netTrafFont = Integer.valueOf((String) objValue);
+            int index = mNetTrafficFont.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.
+                NETWORK_TRAFFIC_FONT_STYLE, netTrafFont);
+            mNetTrafficFont.setSummary(mNetTrafficFont.getEntries()[index]);
             return true;
         } else if (preference == mNetTrafficType) {
             int val = Integer.valueOf((String) objValue);